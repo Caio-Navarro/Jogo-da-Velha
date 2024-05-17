eea2846228da2715ea6c1aaf90ce95fea26fb89b
Gustavo <gustavo.v.santana@ba.estudante.senai.br>
2024-05-17 20:01:26 -0300

    Gustavo

--- Jogo da Velha.cpp
+++ Jogo da Velha.cpp
@@ -14,6 +14,31 @@ struct Jogo{
 	int empates = 0;
 } jogo;
 
-void corrigirAcentuacao(){ //
+void corrigirAcentuacao(){
 	setlocale(LC_ALL, "Portuguese");
 }
+
+void comecaJogo(){
+	printf("\nPressione qualquer tecla para começar o jogo...");
+	getchar();
+}
+
+void creditos(){
+	printf("Feito por: Caio Navarro, Emanuel Lucas, Gustavo, Kauan Brandão e Samuel Falcão.\n\n");
+	system("pause");
+}
+
+void placar(){
+	if(jogo.vezesJogadas == 0){
+		printf("Nenhuma partida aconteceu!\n\n");
+		system("pause");
+		system("cls");
+		menu();
+	}else{
+		printf("Vitórias jogador X: %d\n", jogo.placarX);
+		printf("Vitórias jogador O: %d\n", jogo.placarO);
+		printf("Empates: %d\n\n", jogo.empates);
+		system("pause");
+	}
+}
+
