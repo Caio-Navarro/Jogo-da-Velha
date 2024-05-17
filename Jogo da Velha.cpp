a09b9a1dad4d829f9324da2ae65b7db0ebcf5110
Kauan <kauanbrandao07@gmail.com>
2024-05-17 19:55:51 -0300

    Kauan

--- Jogo da Velha.cpp
+++ Jogo da Velha.cpp
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void menu();
+
+
+struct Jogo{ 
+	char tabuleiro[9];
+	int posicaoEscolhida;
+	int vezesJogadas = 0;
+	int placarX = 0;
+	int placarO = 0;
+	int empates = 0;
+} jogo;
+
+void corrigirAcentuacao(){ //
+	setlocale(LC_ALL, "Portuguese");
+}
